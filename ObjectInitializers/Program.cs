dcbfc750c870cc69bd4ec73d323e9219aa1324a0
Tigris <tabula.tenebris@gmail.com>
2021-09-19 04:32:21 +0600

    Added Rectangle class.

--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -27,6 +27,25 @@ namespace ObjectInitializers
             Point goldPoint = new Point(PointColor.Gold) { X = 90, Y = 20 };
             goldPoint.DisplayStats();
 
+            Rectangle myRect = new Rectangle
+            {
+                TopLeft = new Point(PointColor.Gold) { X = 10, Y = 10 },
+                BottomRight = new Point(PointColor.LightBlue) { X = 200, Y = 200 }
+            };
+            myRect.DisplayStats();
+
+            //traditional approach
+            Rectangle r = new Rectangle();
+            Point p1 = new Point();
+            p1.X = 10;
+            p1.Y = 10;
+            r.TopLeft = p1;
+            Point p2 = new Point();
+            p2.X = 200;
+            p2.Y = 200;
+            r.BottomRight = p2;
+            r.DisplayStats();
+
             Console.WriteLine();
         }
     }
